86bc0ab2befbb97c9902a4a512c6a06a6a005330
Kylie <khanhlinh970@gmail.com>
2017-12-08 01:55:05 +0700

    Change application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Blog
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 4.2
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
