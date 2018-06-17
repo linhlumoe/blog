042514311df994ffdb6496a3c5e2de7b35caa017
Kylie <khanhlinh970@gmail.com>
2018-06-17 13:40:14 +0700

    fix validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-  validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 200 }
+  validates :content, presence: true, length: { minimum: 10 }
   default_scope -> { order(created_at: :desc) }
   scope :published_status, -> { where(status: 1) }
   scope :starts_with, -> (title) { where("title ilike ?", "%#{title}%") }
